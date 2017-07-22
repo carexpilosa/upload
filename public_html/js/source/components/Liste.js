d30cb33faf8708f1ea89c084b7a81484ac9931b9
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-07-22 23:09:27 +0200

    list component: data as comment added

--- public_html/js/source/components/Liste.js
+++ public_html/js/source/components/Liste.js
@@ -60,3 +60,25 @@ Liste.defaultProps = {
 };
 
 export default Liste;
+
+
+
+/*
+'Bundesland', 'Fläche in km²'
+Bayern 	70.549,19
+Niedersachsen 	47.618,24
+Baden-Württemberg 	35.751,65
+Nordrhein-Westfalen 	34.083,52
+Brandenburg 	29.477,16
+Mecklenburg-Vorpommern 	23.174,17
+Hessen 	21.114,72
+Sachsen-Anhalt 	20.445,26
+Rheinland-Pfalz 	19.847,39
+Sachsen 	18.414,82
+Thüringen 	16.172,14
+Schleswig-Holstein 	15.763,18
+Saarland 	2.568,65
+Berlin 	891,75
+Hamburg 	755,16
+Bremen 	404,23
+*/
