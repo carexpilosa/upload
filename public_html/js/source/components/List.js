d4db771a29117664758665b44488dae212d1e88c
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-07-23 14:57:10 +0200

    setting props for List component

--- public_html/js/source/components/List.js
+++ public_html/js/source/components/List.js
@@ -71,25 +71,10 @@ class List extends Component {
 
 
 List.defaultProps = {
-  header: ['Bundesland', 'Fläche in km²'],
+  header: ['Keine Daten übergeben!'],
   sorting: ['abc', 'number'],
   daten: [
-    ['Bayern', 70549.19],
-    ['Berlin', 891.75],
-    ['Schleswig-Holsten', 15763.18],
-    ['Niedersachsen', 47618.24],
-    ['Baden-Württemberg', 35751.65],
-    ['Nordrhein-Westfalen', 34083.52],
-    ['Brandenburg', 29477.16],
-    ['Mecklenburg-Vorpommern', 23174.17],
-    ['Hessen', 21114.72],
-    ['Sachsen-Anhalt', 20445.26],
-    ['Rheinland-Pfalz', 19847.39],
-    ['Sachsen', 18414.82],
-    ['Thüringen', 16172.14],
-    ['Saarland', 2568.65],
-    ['Hamburg', 755.16],
-    ['Bremen', 404.23]
+    ['Bitte Daten an List Componente übergeben!']
   ]
 };
 
--- public_html/js/source/index.js
+++ public_html/js/source/index.js
@@ -5,6 +5,27 @@ import ReactDOM from 'react-dom';
 import Uploader from './components/Uploader';
 import List from './components/List';
 
+let header = ['Bundesland', 'Fläche in km²'],
+    sorting = ['abc', 'number'],
+    daten = [
+      ['Bayern', 70549.19],
+      ['Berlin', 891.75],
+      ['Schleswig-Holsten', 15763.18],
+      ['Niedersachsen', 47618.24],
+      ['Baden-Württemberg', 35751.65],
+      ['Nordrhein-Westfalen', 34083.52],
+      ['Brandenburg', 29477.16],
+      ['Mecklenburg-Vorpommern', 23174.17],
+      ['Hessen', 21114.72],
+      ['Sachsen-Anhalt', 20445.26],
+      ['Rheinland-Pfalz', 19847.39],
+      ['Sachsen', 18414.82],
+      ['Thüringen', 16172.14],
+      ['Saarland', 2568.65],
+      ['Hamburg', 755.16],
+      ['Bremen', 404.23]
+    ];
+
 
 ReactDOM.render(
   <span>
@@ -12,7 +33,7 @@ ReactDOM.render(
     <br/>
     <br/>
     <br/>
-    <List />
+    <List header={header} sorting={sorting} daten={daten}/>
   </span>,
   document.getElementById('app')
 );
