f485212de784af121d00f2ef51292b0879775f80
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-07-24 17:24:32 +0200

    build with webpack; data for liste changed

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@ echo ''
 echo '============= babel: ============='
 babel --presets react,es2015 public_html/js/source/ -d public_html/js/build
 
-echo ''
-echo '============= browserify: ============='
-browserify public_html/js/build/index.js -o public_html/js/app.js
-
 #echo ''
-#echo '============= webpack: ============='
-#webpack public_html/js/build/index.js public_html/js/app.js
+#echo '============= browserify: ============='
+#browserify public_html/js/build/index.js -o public_html/js/app.js
+
+echo ''
+echo '============= webpack: ============='
+webpack public_html/js/build/index.js public_html/js/app.js
--- public_html/js/source/components/List.js
+++ public_html/js/source/components/List.js
@@ -13,6 +13,7 @@ class List extends Component {
       sorting: props.sorting
     };
     this.headOnClick = this.headOnClick.bind(this);
+    this.testFc = this.testFc.bind(this);
   }
 
   render() {
@@ -29,8 +30,8 @@ class List extends Component {
                 return (
                   <tr key={o_idx}>{
                     o_dat.map(function (i_dat, i_idx) {
-                      return <td key={i_idx}>{i_dat}</td>;
-                    })
+                      return <td key={i_idx} data-row={o_idx} onClick={this.testFc}>{i_dat}</td>;
+                    }, this)
                   }</tr>
                 );
               }, this)
@@ -66,6 +67,10 @@ class List extends Component {
       descending: descending
     });
   }
+
+  testFc(event) {
+    console.log(event.target.dataset.row);
+  }
 }
 
 
@@ -100,4 +105,25 @@ Saarland 	2.568,65
 Berlin 	891,75
 Hamburg 	755,16
 Bremen 	404,23
+
+
+Land		Fläche in km2   Ew. insgesamt	Ew. männlich	Ew. weiblich	Ew. je km²
+
+
+['Schleswig-Holstein', 15802.49, 2858714, 1399458, 1459256, 181],
+['Hamburg', 755.30, 1787408, 873062, 914346, 2366],
+['Niedersachsen', 47615.85	7926599	3915398	4011201	166],
+['Bremen', 419.84, 671489, 330895, 340594, 1599],
+['Nordrhein-Westfalen', 34112.52, 17865516, 8768019, 9097497, 524],
+['Hessen', 21114.90, 6176172, 3047730, 3128442, 293],
+['Rheinland-Pfalz', 19854.46, 4052803, 1999333, 2053470, 204],
+['Baden-Württemberg', 35751.33, 10879618, 5393388	5486230, 304],
+['Bayern', 70550.07, 12843514, 6352172, 6491342, 182],
+['Saarland', 2568.69, 995597, 488631, 506966, 388],
+['Berlin', 891.68, 3520031, 1726533, 1793498, 3948],
+['Brandenburg', 29654.36, 2484826, 1228283, 1256543, 84],
+['Mecklenburg-Vorpommern', 23213.74, 1612362, 797832, 814530	69],
+['Sachsen', 18449.39, 4084851, 2011561, 2073290, 221],
+['Sachsen-Anhalt', 20451.74, 2245470, 1106689, 1138781, 110],
+['Thüringen', 16202.38, 2170714, 1075139, 1095575, 134]
 */
